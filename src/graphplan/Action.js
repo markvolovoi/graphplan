a9812192666d9f45f6b50045f99eea0b507034b7
avawandersleben <awandersleben05@gmail.com>
2025-04-15 18:21:04 -0400

    all - I'm bad at commiting a littke

--- src/graphplan/Action.js
+++ src/graphplan/Action.js
@@ -1,13 +1,15 @@
-class Effect {
-	constructor(name, type) {
-		this.name = name;
-		this.type = type; // should either be add effect or delete effect
-	}
+// class Effect {
+// 	constructor(name, type) {
+// 		this.name = name;
+// 		this.type = type; // should either be add effect or delete effect
+// 	}
 
-	toString() {
-		return `${this.type} Effect ${this.name}`;
-	}
-}
+// 	toString() {
+// 		return `${this.type} Effect ${this.name}`;
+// 	}
+// }
+
+//Instead of effect I think we should just use propositions for effects
 
 class Action {
 	constructor(name, params, preconditions, effects) {
@@ -17,9 +19,32 @@ class Action {
 		this.effects = effects;
 	}
 
-	isApplicable(prevLevel) {}
+	isApplicable(prevLevel) {
+		let preconditions_left = structuredClone(this.preconditions);
+		for (prop in prevLevel.propositions) {
+			if (preconditions_left.contains(prop)) {
+				let index = preconditions_left.indexOf(prop);
+				preconditions_left.splic(index, 1);
+			}
+			if (preconditions_left.length == 0) {
+				return true;
+			}
+		}
+		return preconditions_left.length == 0;
+	}
 
-	apply() {}
+	//take in a list of propositions and return a list of new propositions after action is done
+	apply(old_propositions) {
+		for (proposition in old_propositions) {
+			for (effect in this.effects) {
+				if (proposition.negation(effect)) {
+					let index = old_propositions.indexOf(propostition);
+					old_propositions.splice(index, 1);
+					old_propositions.append(effect);
+				}
+			}
+		}
+	}
 
 	toString() {
 		return `Action ${this.name} (${this.params})`;
--- src/graphplan/Proposition.js
+++ src/graphplan/Proposition.js
@@ -1,11 +1,16 @@
 class Proposition {
-	constructor(name, params) {
+	constructor(name, params, truth_value) {
 		this.name = name;
 		this.params = params;
+		this.truth_value = truth_value;
 	}
 
 	equals(other) {
-		return this.name === other.name && this.params === other.params;
+		return this.name === other.name && this.params === other.params && this.truth_value == other.truth_value;
+	}
+
+	negation(other) {
+		return this.name === other.name && this.params === other.params && this.truth_value == !other.truth_value;
 	}
 
 	toString() {
